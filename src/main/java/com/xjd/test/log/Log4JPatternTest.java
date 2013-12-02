3d16a7da998dc1c0abb31fd363eb342617f99fc1
elvis9xu <elvis9xu@gmail.com>
2013-12-02 17:41:36 +0800

    log4j pattern

---
+++ src/main/java/com/xjd/test/log/Log4JPatternTest.java
@@ -0,0 +1,21 @@
+package com.xjd.test.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log4JPatternTest {
+	private static final Logger log = LoggerFactory.getLogger(Log4JPatternTest.class);
+
+	/**
+	 * <pre>
+	 * 
+	 * </pre>
+	 * @param args
+	 * @author elvis.xu
+	 * @since 2013-11-28
+	 */
+	public static void main(String[] args) {
+		log.info("HELLO");
+	}
+
+}
