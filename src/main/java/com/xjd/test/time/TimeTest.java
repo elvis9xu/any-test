ac47ea3b5f45662b5d7aeca9595b3ddac4d0cd8f
elvis <elvis9xu@163.com>
2015-05-04 16:02:03 +0800

    中文字拼音

--- src/main/java/com/xjd/test/time/TimeTest.java
+++ src/main/java/com/xjd/test/time/TimeTest.java
@@ -1,15 +1,24 @@
 package com.xjd.test.time;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TimeTest {
 
 	public static void main(String[] args) {
-		long t = 4294967295L;
-
-		System.out.println(System.currentTimeMillis());
-		System.out.println(t * 1000);
-		System.out.println(new Date(t * 1000L));
+//		long t = 4294967295L;
+//
+//		System.out.println(System.currentTimeMillis());
+//		System.out.println(t * 1000);
+//		System.out.println(new Date(t * 1000L));
+		
+		
+		Date now = new Date();
+		System.out.println(now.getTime());
+		
+		Date d = new Date(now.getTime());
+		String s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(d);
+		System.out.println(s);
 	}
 
 }
